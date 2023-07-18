92b296e0169775f580c6b8612aac227a279cde0a
Obi James Onyemaechi <jamesobi@softprism.org>
2023-07-18 19:13:30 +0100

    minor improvement

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,8 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @IsNotEmpty()
   password: string;
   @IsString()
+  @IsNotEmpty()
   username: string;
 }
--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,8 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class SigninDto {
   @IsString()
+  @IsNotEmpty()
   password: string;
   @IsString()
+  @IsNotEmpty()
   username: string;
 }
